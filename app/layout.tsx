abc0b624e50b19fbc2b2450836322c6b1a84dd15
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-10 15:19:51 -0500

    bg-slate 500 tailwind change

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="bg-slate-500">
+          {children}
+        </div>
       </body>
     </html>
   );
