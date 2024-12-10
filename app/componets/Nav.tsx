87c40d0709dba0b8b42344b946a6a73df1caaf86
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-10 17:47:10 -0500

    created nav outside of folder

---
+++ app/componets/Nav.tsx
@@ -0,0 +1,11 @@
+'use client'
+import Link from "next/link";
+
+export default function Nav(){
+    return(
+        
+        <Link href="./contact">
+                Send Emails
+        </Link>
+    )
+}
