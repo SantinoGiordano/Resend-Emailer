672dc91711c4a9b0a1fc16b451e8ae75c5667e9a
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-10 18:11:33 -0500

    created simple back button

---
+++ app/componets/FixedButton.tsx
@@ -0,0 +1,7 @@
+import Link from "next/link";
+
+export default function FixedButton(){
+    return(
+        <Link href={'./'} className="fixed bottom-5 right-5 bg-blue-500 text-white px-6 py-3 rounded-lg text-lg hover:bg-blue-700 focus:outline-none">Back</Link>
+    )
+}
