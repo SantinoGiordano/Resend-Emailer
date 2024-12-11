3117a2ba1875f115940cdc0eb04501b57c4820a6
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-11 09:50:22 -0500

    linked button lined

--- app/componets/FixedButton.tsx
+++ app/componets/FixedButton.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 
 export default function FixedButton(){
     return(
-        <Link href={'./'} className="fixed bottom-5 right-5 bg-blue-500 text-white px-6 py-3 rounded-lg text-lg hover:bg-blue-700 focus:outline-none">Back</Link>
+        <Link href={'./'} className="fixed bottom-5 right-5 bg-blue-500 text-white px-6 py-3 rounded-lg text-lg hover:bg-blue-700 focus:outline-none">
+        Back
+        </Link>
     )
 }
